62bf3c318d889bb61a16ed7002221afb458f1e6e
yy <yynane@gmail.com>
2016-10-28 14:11:57 +0800

    add video_model

---
+++ test_daily/201610/video_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class UserAction(object):
+    pass
+
+
+class VideoAnalyzer(object):
+
+    def analyzer_action(self):
+        pass
+
+
+class LetvAnalyzer(VideoAnalyzer):
+    pass
+
+
+class YoukuAnalyzer(VideoAnalyzer):
+    pass
+
+
+def to_db():
+    pass
+
+
+def write_log(event_class, path):
+    pass
+
+

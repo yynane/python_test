2eb1723982edf141120cfbbdb2da9c59c3739de5
yy <yynane@gmail.com>
2016-11-17 20:13:51 +0800

    add csv merge

---
+++ test_daily/201611/data_merge.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@version: 
+@author: yuying
+@file: data_merge.py
+@time: 2016/11/17 14:39
+"""
+
+import subprocess
+import csv
+
+cmd1 = subprocess.getstatusoutput(r'dir .\data /b')[1]
+file_list = cmd1.splitlines()
+file_all = csv.writer(open(r'.\data\all.csv', 'w'))
+
+for i in file_list:
+    file_csv = open(r'.\data\%s' % i, 'r')
+    for v in csv.reader(file_csv):
+        if "city_id" not in v[0]:
+            file_all.writerow(v)
+    file_csv.close()
+
+
